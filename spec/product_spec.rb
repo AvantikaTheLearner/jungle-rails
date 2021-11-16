d39ca1ea4bf10ea43491fee7b03293779711053e
AvantikaTheLearner <avantikaagarwaljhs@gmail.com>
2021-11-16 14:53:19 -0500

    Small changes in product_spec.rb

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,41 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  let(:category) {
-    Category.new(name: "testing")
- }
- subject {
-    category.products.new(
-      name: "test",
-      description: "for RSpec",
-      price_cents: 2000,
-      quantity: 20,
-    )
- }
+  let(:category) { Category.new(name: "testing") }
+  subject(:products) do
+      category.products.new(
+        name: "test",
+        description: "for RSpec",
+        price_cents: 2000,
+        quantity: 20,
+      )
+  end
   describe 'Validations' do
     it "is valid with valid attributes" do
-      expect(subject).to be_valid
-      expect(subject.errors.full_messages).to be_empty
+      expect(products).to be_valid
+      expect(products.errors.full_messages).to be_empty
     end
     it "is not valid without a name" do
-      subject.name = nil
-      expect(subject).to_not be_valid
-      expect(subject.errors.full_messages).to include "Name can't be blank"
+      products.name = nil
+      expect(products).to_not be_valid
+      expect(products.errors.full_messages).to include "Name can't be blank"
     end
     it "is not valid without a price" do
-      subject.price_cents = nil
-      expect(subject).to_not be_valid
-      expect(subject.errors.full_messages).to include "Price can't be blank"
+      products.price_cents = nil
+      expect(products).to_not be_valid
+      expect(products.errors.full_messages).to include "Price can't be blank"
     end
     it "is not valid without a quantity" do
-      subject.quantity = nil
-      expect(subject).to_not be_valid
-      expect(subject.errors.full_messages).to include "Quantity can't be blank"
+      products.quantity = nil
+      expect(products).to_not be_valid
+      expect(products.errors.full_messages).to include "Quantity can't be blank"
     end
     it "is not valid without a category" do
-      subject.category = nil
-      expect(subject).to_not be_valid
-      expect(subject.errors.full_messages).to include "Category can't be blank"
+      products.category = nil
+      expect(products).to_not be_valid
+      expect(products.errors.full_messages).to include "Category can't be blank"
     end
   end
 end
